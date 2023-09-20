1aa6561315b2d745ce748888d78d7854fd4890b4
Ullyana <144201909+Ullyana@users.noreply.github.com>
2023-09-20 16:58:13 +0500

    задание 1 2 1.py

--- 1.py
+++ 1.py
@@ -1,15 +1,21 @@
-import math as m
-x=float(input())
-y=float(input())
-z=float(input())
-a=m.sqrt(x*y/(x+y)*(x-y))-PI/2
-b=(abs(x))**(1/5)+m.sin(y)+m.atan(x)-log10(1+z)
+from cmath import *
+
+def task1():
+  pass
+#задание 1
+x = float(input("Введите переменную x:"))
+y = float(input("Введите переменную y:"))
+z = float(input("Введите переменную z:"))
+a = sqrt(x*y/(x+y)*(x-y))-pi/2
+b = (abs(x))**(1/5)+sin(y)+atan(x)-log10(1+z)
 print('a=',a)
 print('b=',b)
 
-import math as m
-x=float(input())
-a=1
-b=-3
-f=(((x)**2)**(1/3)/x+a)+x**b
+#задание 2
+x = float(input("Введите переменную x:"))
+a = 1
+b = -3
+f = (((x)**2)**(1/3)/x+a)+x**b
+print('f=',f)
 
+#задание 3
