226b9a4cabe32670de4d85b9f798b5cd369cb88b
Ullyana <144201909+Ullyana@users.noreply.github.com>
2024-02-22 22:06:40 +0500

    Update lab4.py

--- labs/lab4.py
+++ labs/lab4.py
@@ -1,39 +1,4 @@
-import matplotlib.pyplot as plt
-import numpy as np
 
-def lab4():
-    x = np.linspace(-10, 10, 400)
-    y = np.linspace(-10, 10, 400)
-    X, Y = np.meshgrid(x, y)
-
-    one = (Y <= 3 * X - 5) & (Y <= -3 * X + 13) & (Y >= -0.5 * X + 2)
-    two = ((X - 4) ** 2 + (Y + 1) ** 2 <= 4) & (Y <= 2 * X - 8) & (X >= 3)
-    three = (Y <= X - 7) & (Y >= X - 10) & (Y <= -2 * X + 11) & (Y >= -2 * X - 5)
-    four = (X >= 4) & (X <= 5) & ((X - 4) ** 2 + (Y + 1) ** 2 <= 4)
-    five = (Y <= -X + 1) & ((X - 4) ** 2 + (Y + 1) ** 2 <= 4)
-    six = (Y <= 5) & (Y >= 1) & (Y >= 1.5 * X + 2) & (X >= -1) & (Y <= 5)
-    seven = (Y <= 0.5 * X + 2) & (Y <= -X + 2) & (Y >= -0.25 * X + 0.5)
-    eight = (Y <= -X - 1) & (Y <= -3 * X - 1) & (Y >= -1.5 * X - 2.5) & (Y <= 1.5)
-    nine = ((X + 1) ** 2 + (Y - 1) ** 2 <= 4) & (Y >= 1) & (X <= -1)
-    ten = ((X + 1) ** 2 + (Y - 1) ** 2 <= 4) & (Y <= -X - 1) & (Y <= 1)
-
-    figure1 = one | two | three | four | five
-    figure2 = six | seven | eight | nine | ten
-
-    plt.figure()
-    plt.contourf(X, Y, figure1, colors='green', alpha=0.5)
-    plt.contourf(X, Y, figure2, colors='blue', alpha=0.5)
-
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title('Фигуры')
-    plt.grid(True)
-    plt.show()
-
-lab4()
-
-
-НЕ ХВАТАЕТ ФУНКЦИИ ОПРЕДЕЛЕНИЯ ВХОЖДЕНИЯ ТОЧКИ В ОБЛАСТЬ
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
@@ -45,7 +10,7 @@ class GraphWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("График")
-        self.setGeometry(100, 100, 800, 600)
+        self.setGeometry(100, 100, 800, 700)
 
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
@@ -66,8 +31,11 @@ class GraphWindow(QMainWindow):
         layout.addWidget(self.entry_y)
 
         self.button_add = QPushButton("Добавить точку")
-        self.button_add.clicked.connect(self.add_point)
+        self.button_add.clicked.connect(self.add_point_in_figure)
         layout.addWidget(self.button_add)
+
+        self.result_label = QLabel()
+        layout.addWidget(self.result_label)
 
         self.x_line1 = [-1, 0, 1, 0, -2]
         self.y_line1 = [-1, -2.5, -4, -1, 1]
@@ -83,199 +51,89 @@ class GraphWindow(QMainWindow):
         self.ax.plot(self.x_line3, self.y_line3, linestyle='-')
         self.ax.plot(self.x_line4, self.y_line4, linestyle='-')
 
-        # Задаем центр и радиус окружности
         center = (-1, 1)
         radius = 2
-        # Задаем угловой диапазон для отображения дуги (например, от 45 градусов до 315 градусов)
         start_angle = np.radians(90)
         end_angle = np.radians(270)
-        # Создаем массив углов в заданном диапазоне
         angles = np.linspace(start_angle, end_angle, 100)
-        # Вычисляем координаты точек окружности в заданном диапазоне углов
         self.x_circle1 = center[0] + radius * np.cos(angles)
         self.y_circle1 = center[1] + radius * np.sin(angles)
-        # Строим график
-        plt.figure(figsize=(6, 6))  # Размер графика
-        # Строим дугу окружности
         self.ax.plot(self.x_circle1, self.y_circle1, linestyle='-')
 
-        # Задаем центр и радиус окружности
         center = (4, -1)
         radius = 2
-        # Задаем угловой диапазон для отображения дуги (например, от 45 градусов до 315 градусов)
         start_angle = np.radians(180)
         end_angle = np.radians(270)
-        # Создаем массив углов в заданном диапазоне
         angles = np.linspace(start_angle, end_angle, 100)
-        # Вычисляем координаты точек окружности в заданном диапазоне углов
         self.x_circle2 = center[0] + radius * np.cos(angles)
         self.y_circle2 = center[1] + radius * np.sin(angles)
-        # Строим дугу окружности
         self.ax.plot(self.x_circle2, self.y_circle2, linestyle='-')
 
-        # Задаем центр и радиус окружности
         center = (4, -1)
         radius = 2
-        # Задаем угловой диапазон для отображения дуги (например, от 45 градусов до 315 градусов)
         start_angle = np.radians(0)
         end_angle = np.radians(90)
-        # Создаем массив углов в заданном диапазоне
         angles = np.linspace(start_angle, end_angle, 100)
-        # Вычисляем координаты точек окружности в заданном диапазоне углов
         self.x_circle3 = center[0] + radius * np.cos(angles)
         self.y_circle3 = center[1] + radius * np.sin(angles)
-        # Строим дугу окружности
         self.ax.plot(self.x_circle3, self.y_circle3, linestyle='-')
 
         self.ax.set_xlabel('X')
         self.ax.set_ylabel('Y')
         self.ax.set_title('График')
+        self.ax.grid(True)
 
-    def add_point(self):
+        self.current_point = None
+
+    def add_point(self, x, y):
+        self.result_label.clear()
+
+        if self.current_point:
+            self.current_point.remove()
+
+        self.current_point, = self.ax.plot(x, y, 'ro')
+
+        self.canvas.draw()
+
+    def add_point_in_figure(self):
+        x_input = self.entry_x.text()
+        y_input = self.entry_y.text()
+
         try:
-            x = float(self.entry_x.text())
-            y = float(self.entry_y.text())
-            self.ax.plot(x, y, marker='o', markersize=8, color='red')
-            self.canvas.draw()
+            x = float(x_input)
+            y = float(y_input)
         except ValueError:
-            pass
+            self.result_label.setText("Введите корректные числа для координат X и Y")
+            return
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = GraphWindow()
-    window.show()
-    sys.exit(app.exec_())
+        self.add_point(x, y)
+
+        if self.check_point_in_figure(x, y):
+            self.result_label.setText("Точка попала в фигуру")
+        else:
+            self.result_label.setText("Точка вне фигуры")
+
+    def check_point_in_figure(self, x, y):
+        one = (y <= 3 * x - 5) and (y <= -3 * x + 13) and (y >= -0.5 * x + 2)
+        two = ((x - 4) ** 2 + (y + 1) ** 2 <= 4) and (y <= 2 * x - 8) and (x >= 3)
+        three = (y <= x - 7) and (y >= x - 10) and (y <= -2 * x + 11) and (y >= -2 * x - 5)
+        four = (x >= 4) and (x <= 5) and ((x - 4) ** 2 + (y + 1) ** 2 <= 4)
+        five = (y <= -x + 1) and ((x - 4) ** 2 + (y + 1) ** 2 <= 4)
+        six = (y <= 5) and (y >= 1) and (y >= 1.5 * x + 2) and (x >= -1) and (y <= 5)
+        seven = (y <= 0.5 * x + 2) and (y <= -x + 2) and (y >= -0.25 * x + 0.5)
+        eight = (y <= -x - 1) and (y <= -3 * x - 1) and (y >= -1.5 * x - 2.5) and (y <= 1.5)
+        nine = ((x + 1) ** 2 + (y - 1) ** 2 <= 4) and (y >= 1) and (x <= -1)
+        ten = ((x + 1) ** 2 + (y - 1) ** 2 <= 4) and (y <= -x - 1) and (y <= 1)
+
+        figure1 = one or two or three or four or five
+        figure2 = six or seven or eight or nine or ten
+
+        if figure1 or figure2:
+            return True
+        else:
+            return False
 
 
-import sys
-import numpy as np
-import matplotlib.pyplot as plt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel, QLineEdit, QPushButton
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-
-class GraphWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("График")
-        self.setGeometry(100, 100, 800, 600)
-
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-        layout = QVBoxLayout(central_widget)
-
-        self.figure, self.ax = plt.subplots()
-        self.canvas = FigureCanvas(self.figure)
-        layout.addWidget(self.canvas)
-
-        self.label_x = QLabel("X:")
-        layout.addWidget(self.label_x)
-        self.entry_x = QLineEdit()
-        layout.addWidget(self.entry_x)
-
-        self.label_y = QLabel("Y:")
-        layout.addWidget(self.label_y)
-        self.entry_y = QLineEdit()
-        layout.addWidget(self.entry_y)
-
-        self.button_add = QPushButton("Добавить точку")
-        self.button_add.clicked.connect(self.check_point_in_figure)
-        layout.addWidget(self.button_add)
-
-        self.x_line1 = [-1, 0, 1, 0, -2]
-        self.y_line1 = [-1, -2.5, -4, -1, 1]
-        self.x_line2 = [-2, 2, 0, 2, -1, -1]
-        self.y_line2 = [1, 0, 2, 5, 5, 3]
-        self.x_line3 = [4, 3, 5, 7, 6]
-        self.y_line3 = [-3, -4, -5, -3, -1]
-        self.x_line4 = [4, 3, 2, 4, 3, 2]
-        self.y_line4 = [1, 4, 1, 0, -2, -1]
-
-        self.ax.plot(self.x_line1, self.y_line1, linestyle='-')
-        self.ax.plot(self.x_line2, self.y_line2, linestyle='-')
-        self.ax.plot(self.x_line3, self.y_line3, linestyle='-')
-        self.ax.plot(self.x_line4, self.y_line4, linestyle='-')
-
-        # Задаем центр и радиус окружности
-        center = (-1, 1)
-        radius = 2
-        # Задаем угловой диапазон для отображения дуги (например, от 45 градусов до 315 градусов)
-        start_angle = np.radians(90)
-        end_angle = np.radians(270)
-        # Создаем массив углов в заданном диапазоне
-        angles = np.linspace(start_angle, end_angle, 100)
-        # Вычисляем координаты точек окружности в заданном диапазоне углов
-        self.x_circle1 = center[0] + radius * np.cos(angles)
-        self.y_circle1 = center[1] + radius * np.sin(angles)
-        # Строим график
-        plt.figure(figsize=(6, 6))  # Размер графика
-        # Строим дугу окружности
-        self.ax.plot(self.x_circle1, self.y_circle1, linestyle='-')
-
-        # Задаем центр и радиус окружности
-        center = (4, -1)
-        radius = 2
-        # Задаем угловой диапазон для отображения дуги (например, от 45 градусов до 315 градусов)
-        start_angle = np.radians(180)
-        end_angle = np.radians(270)
-        # Создаем массив углов в заданном диапазоне
-        angles = np.linspace(start_angle, end_angle, 100)
-        # Вычисляем координаты точек окружности в заданном диапазоне углов
-        self.x_circle2 = center[0] + radius * np.cos(angles)
-        self.y_circle2 = center[1] + radius * np.sin(angles)
-        # Строим дугу окружности
-        self.ax.plot(self.x_circle2, self.y_circle2, linestyle='-')
-
-        # Задаем центр и радиус окружности
-        center = (4, -1)
-        radius = 2
-        # Задаем угловой диапазон для отображения дуги (например, от 45 градусов до 315 градусов)
-        start_angle = np.radians(0)
-        end_angle = np.radians(90)
-        # Создаем массив углов в заданном диапазоне
-        angles = np.linspace(start_angle, end_angle, 100)
-        # Вычисляем координаты точек окружности в заданном диапазоне углов
-        self.x_circle3 = center[0] + radius * np.cos(angles)
-        self.y_circle3 = center[1] + radius * np.sin(angles)
-        # Строим дугу окружности
-        self.ax.plot(self.x_circle3, self.y_circle3, linestyle='-')
-
-        self.ax.set_xlabel('X')
-        self.ax.set_ylabel('Y')
-        self.ax.set_title('График')
-
-    def check_point_in_figure(x, y):
-        try:
-            x = float(self.entry_x.text())
-            y = float(self.entry_y.text())
-            self.ax.plot(x, y, marker='o', markersize=8, color='red')
-            self.canvas.draw()
-
-            one = (y <= 3 * x - 5) and (y <= -3 * x + 13) and (y >= -0.5 * x + 2)
-            two = ((x - 4) ** 2 + (y + 1) ** 2 <= 4) and (y <= 2 * x - 8) and (x >= 3)
-            three = (y <= x - 7) and (y >= x - 10) and (y <= -2 * x + 11) and (y >= -2 * x - 5)
-            four = (x >= 4) and (x <= 5) and ((x - 4) ** 2 + (y + 1) ** 2 <= 4)
-            five = (y <= -x + 1) and ((x - 4) ** 2 + (y + 1) ** 2 <= 4)
-            six = (y <= 5) and (y >= 1) and (y >= 1.5 * x + 2) and (x >= -1) and (y <= 5)
-            seven = (y <= 0.5 * x + 2) and (y <= -x + 2) and (y >= -0.25 * x + 0.5)
-            eight = (y <= -x - 1) and (y <= -3 * x - 1) and (y >= -1.5 * x - 2.5) and (y <= 1.5)
-            nine = ((x + 1) ** 2 + (y - 1) ** 2 <= 4) and (y >= 1) and (x <= -1)
-            ten = ((x + 1) ** 2 + (y - 1) ** 2 <= 4) and (y <= -x - 1) and (y <= 1)
-
-            figure1 = one or two or three or four or five
-            figure2 = six or seven or eight or nine or ten
-
-            if figure1 or figure2:
-                return True
-            else:
-                return False
-
-            if check_point_in_figure(x, y):
-                print(f"Точка ({x}, {y}) входит в фигуру.")
-            else:
-                print(f"Точка ({x}, {y}) не входит в фигуру.")
-        except ValueError:
-            pass
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = GraphWindow()
